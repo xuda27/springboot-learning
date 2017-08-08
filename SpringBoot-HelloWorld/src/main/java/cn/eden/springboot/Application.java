85aace2ee40c08948b9425f6e132d1242a84c3b1
xuda <xuda27@163.com>
2017-08-08 22:56:46 +0800

    首次提交

---
+++ SpringBoot-HelloWorld/src/main/java/cn/eden/springboot/Application.java
@@ -0,0 +1,18 @@
+package cn.eden.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot 启动类
+ * @author eden
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
